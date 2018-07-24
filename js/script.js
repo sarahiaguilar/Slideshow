d4dd5c230d37166a0be6ee0fdfa18793ba1230e5
Sarahí Aguilar <sarahiaguilargonzalez@gmail.com>
2018-07-24 11:26:50 -0400

    Auto slide

--- js/script.js
+++ js/script.js
@@ -8,7 +8,9 @@ var p = {
 	animacionSlide: "slide",
 	imgSlide: document.querySelectorAll("#slide ul li"),
 	avanzar: document.querySelector("#slide #avanzar"),
-	retroceder: document.querySelector("#slide #retroceder")
+	retroceder: document.querySelector("#slide #retroceder"),
+	velocidadSlide: 3000,
+	formatearLoop: false
 }
 
 /*=============================================
@@ -23,6 +25,8 @@ var m = {
 
 		p.avanzar.addEventListener("click", m.avanzar);
 		p.retroceder.addEventListener("click", m.retroceder);
+
+		m.intervalo();
 	},
 
 	paginacionSlide: function(item) {
@@ -49,6 +53,8 @@ var m = {
 	},
 
 	movimientoSlide: function(item) {
+		p.formatearLoop = true;
+
 		p.cajaSlide.style.left = item * -100 + "%";
 		for (var i = 0; i < p.paginacion.length; i++) {
 
@@ -67,6 +73,16 @@ var m = {
 				p.imgSlide[item].style.opacity = 1;
 			}, 500);
 		}
+	},
+
+	intervalo: function() {
+		setInterval(function() {
+			if (p.formatearLoop) {
+				p.formatearLoop = false;
+			} else {
+				m.avanzar();
+			}
+		}, p.velocidadSlide)
 	}
 }
 
